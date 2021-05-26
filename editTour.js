b0d52e24328f85120f0872e3916d5663218a04a3
Beakdoson <jelani.hurtault85@gmail>
2021-05-26 11:05:42 -0700

    Changes to edit and script file

--- editTour.js
+++ editTour.js
@@ -1,35 +1,67 @@
-// function to capture button clicks for edit and delete
+// dependencies to run services
+const express = require(`express`);
+const cors = require("cors");
+const fetch = require("node-fetch");
 
-document.getElementById("btn_add").addEventListener("click", (addTourCard) => {
-  // prevent default action of the webpage
-  addTourCard.preventDefault();
-  // object to capture click event
-  let addBtn = addTourCard.target;
+// obj to use express function
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-  // console log tour added
-  console.log("Tour has been added");
+// obj's to pull brewery list from Heroku and Api brewery
+const savedListUrl = `https://bjj-byob.herokuapp.com/list`;
+const apiBase = `https://api.openbrewerydb.org/breweries/`;
 
-  // create p tag to append new tour added
-  const newTour = document.getElementById("new_tour");
+// fetch function to get saved list from database
+fetch(savedListUrl)
+  .then((response) => response.json())
+  .then((savedToursList) => fetchToursList(savedToursList));
 
-  // creates new p tag for card if customer adds tour
-  newTour.innerHTML += `
-  <p>Did this work?</p>
-  <button type"button">Add</button>
-  `;
+function fetchToursList(savedToursList) {
+  for (tours of savedToursList) {
+    createTourCard(tours);
+    console.log(tours);
+  }
+}
 
-  //   document.getElementById("btn_add").appendChild(newTour);
+// creating a server for web app to contact backend database
+app.listen(3000, function () {
+  console.log(`Listening on 3000`);
 });
-document
-  .getElementById("btn_delete")
-  .addEventListener("click", (deleteTourCard) => {
-    // prevent the default action of webpage
-    deleteTourCard.preventDefault();
-    // capture event of the webpage
-    let deleteBtn = deleteTourCard.target();
 
-    // deletes new_tour p tag
-    const delTour = document.getElementById("new_tour");
+// get function to receive list of breweries
 
-    delTour.innerHTML -= `<p>${newTour}</p>`;
-  });
+// // function to capture button clicks for edit and delete
+// document.getElementById("btn_add").addEventListener("click", (addTourCard) => {
+//   // prevent default action of the webpage
+//   addTourCard.preventDefault();
+//   // object to capture click event
+//   let addBtn = addTourCard.target;
+
+//   // console log tour added
+//   console.log("Tour has been added");
+
+//   // create p tag to append new tour added
+//   const newTour = document.getElementById("new_tour");
+
+//   // creates new p tag for card if customer adds tour
+//   newTour.innerHTML += `
+//   <p>Did this work?</p>
+//   <button type"button">Add</button>
+//   `;
+
+//   //   document.getElementById("btn_add").appendChild(newTour);
+// });
+// document
+//   .getElementById("btn_delete")
+//   .addEventListener("click", (deleteTourCard) => {
+//     // prevent the default action of webpage
+//     deleteTourCard.preventDefault();
+//     // capture event of the webpage
+//     let deleteBtn = deleteTourCard.target();
+
+//     // deletes new_tour p tag
+//     const delTour = document.getElementById("new_tour");
+
+//     delTour.innerHTML -= `<p>${newTour}</p>`;
+//   });
